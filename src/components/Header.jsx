18dc74da3b89caa437cf92c0711d6e45d69b1de7
Gerardo_RJI <gera.rji96@gmail.com>
2026-08-14 05:32:18 -0600

    Refactor shopping cart logic into custom `useCart` hook for cleaner and reusable code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,4 @@
-import { useMemo } from "react";
-
-export default function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart }) {
-
-  // State Derivado
-  const isEmpty = useMemo(() => cart.length === 0, [cart]);
-  const cartTotal = useMemo(() => cart.reduce((acc, item) => acc + (item.quantity * item.price), 0), [cart]);
+export default function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal }) {
 
   return (
     <>
